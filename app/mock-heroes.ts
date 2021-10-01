d9a92eb8087e74057bd3f14c1e3d0761eedd6765
Fahd <78287710+cfahd@users.noreply.github.com>
2021-10-01 19:24:29 -0400

    Add files via upload

---
+++ app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Fahd', description: 'Project Manager, Server Side Programmer & Builder'},
+  { id: 2, name: 'Umme' , description: 'Scrum Master'},
+  { id: 3, name: 'Reina' , description: 'Client Side Programmer'},
+  { id: 4, name: 'Stacy' , description: 'Client Side Programmer'},
+  { id: 5, name: 'Vy' , description: 'Database Designer & Programmer'},
+  { id: 6, name: 'Robert' , description: 'Database Designer & Programmer'}
+];
